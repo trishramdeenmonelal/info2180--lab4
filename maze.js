668d45f0cf046f28cf3ad5bb9e32ac72c74dae65
trishramdeenmonelal <43709082+trishramdeenmonelal@users.noreply.github.com>
2018-10-18 21:32:32 -0400

    Attempt at Ex 2

--- maze.js
+++ maze.js
@@ -1,17 +1,22 @@
 window.onload= function(){
 	var x= document.getElementById("boundary1");
 	x.onmouseover = changeColour;
-
-
-
-
+	var b=document.querySelectorAll('.boundary');
+	var i;
+	b.onmouseover= changeAllColours;
+	
+	
 	function changeColour(){
-		x.classList.toggle("youlose");
-		alert("you lose");
+		x.classList.add("youlose");
+		alert("you lose");	
 	}
-
+	
+	function changeAllColours(){
+		for(i=0; i < b.length; i++){
+			b[i].classList.add("youlose");
+			alert("you lose")
+		}
+	}
 }
-
-
 
 
